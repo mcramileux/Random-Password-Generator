c68d1c7343cefa8146eee04ce92d9e7162f35e51
Kristine Ramilo <kristineramilo@192-168-1-114.tpgi.com.au>
2023-03-31 13:28:52 +1000

    updated javascript

---
+++ script.js
@@ -0,0 +1,60 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//Set a password length and complexity
+var lengthElement = document.querySelector ("#length");
+var lowerCaseCharacters = document.querySelector ("#lowercase");
+var upperCaseCharacters = document.querySelector ("#uppercase");
+var numberCharacters = document.querySelector ("#numbercase");
+var specialCharacters = document.querySelector ("#special");
+
+// The password characters set to be used
+var lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+var upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numberCharacters = '0123456789';
+var specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+// Generate Random and Strong Password from 8 to 128 characters
+function generatePassword() {
+    passwordLength = prompt ("Please enter you password length between 8 to 128 characters.");
+    includeLowerCaseCharacters = confirm("Would you like to include lowercase characters?");
+    includeUpperCaseCharacters = confirm ("Would you like to include uppercase characters?");
+    includeNumberCharacters = confirm ("Would you like to include number characters?");
+    includeSpecialCharacters = confirm ("Would you like to include special characters?");
+    console.log(passwordLength);
+  
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength === null) {
+      confirm ("Please try again. The password must have at least 8 characters and not more than 128 characters");
+    }
+  
+  // let length = lengthElement.value;
+  let charSet = "";
+
+  // Build the character set from the selected options
+  if (includeLowerCaseCharacters) charSet += lowerCaseCharacters;
+  if (includeUpperCaseCharacters) charSet += upperCaseCharacters;
+  if (includeNumberCharacters) charSet += numberCharacters;
+  if (includeSpecialCharacters) charSet += specialCharacters;
+    
+  let password = "";
+  for (let i = 0; i < passwordLength ; i ++){
+  password += charSet.charAt(Math.floor(Math.random()* charSet.length))
+  }
+  return password
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
